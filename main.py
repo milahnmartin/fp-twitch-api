2c625ffb9ca2491f0b429709c175658c21965966
milahnmartin <milahnmartin.develop@gmail.com>
2021-07-21 00:27:17 +0200

    connected main.py with faceit.py , can now make requests, responds with all the details

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ import webscraper
 app = Flask(__name__)
 
 
-@app.route('/get/<string:user_name>/<string:server_name>/<string:category>')
-def get(user_name: str, server_name: str, category: str) -> str:
+@app.route('/get/<string:user_name>/<string:server_name>')
+def get(user_name: str, server_name: str) -> str:
+    server_name.lower()
     if server_name == 'faceit':
-        return webscraper.faceit_get(user_name, category)
+        user_instance = webscraper.Faceit(user_name)
+        return user_instance.get_player_stats()
     elif server_name == 'esea':
         return webscraper.esea_get(user_name, category)
     elif server_name == 'hltv':
@@ -16,5 +18,9 @@ def get(user_name: str, server_name: str, category: str) -> str:
         return f'<h1>Following Server {server_name} is out of Service ...</h1>'
 
 
+@app.route('/')
+def home()-> dict:
+    return {"status":"error","reason":"no url given"}
+
 if __name__ == "__main__":
-    app.run(ssl_context='adhoc')
+    app.run()
--- webscraper/faceit.py
+++ webscraper/faceit.py
@@ -1,44 +1,42 @@
 import requests
 from requests import api
 from requests.api import head
-# from webscraper.keys import my_api_key
-import keys
+from webscraper.keys import my_api_key
+# import keys
 
 
 class Faceit:
 
     game_id = "csgo"
-    api_header = {"Authorization":"Bearer " + keys.my_api_key,"content-type": "application/json"}
+    api_header = {"Authorization":"Bearer " + my_api_key,"content-type": "application/json"}
 
 
-    def __init__(self,pNickname) -> None:
-        self.pNickname = pNickname
+    def __init__(self,pname) -> None:
+        self.pname = pname
        
         
 
-    def get_player_id(self):
-        player_id_request = requests.get(f"https://open.faceit.com/data/v4/players?nickname={self.pNickname}&game=CSGO",headers=self.api_header)
+    def get_player_id(self) -> int:
+        player_id_request = requests.get(f"https://open.faceit.com/data/v4/players?nickname={self.pname}&game=CSGO",headers=self.api_header)
         player_id_data = player_id_request.json()
         player_id = player_id_data["player_id"]
         return player_id
 
 
-    def get_player_data(self):
+    def get_player_data(self) -> dict:
         player_data = requests.get(f"https://open.faceit.com/data/v4/players/{self.get_player_id()}",headers=self.api_header)
         player_data_json = player_data.json()
-        print(player_data_json)
-    
+        return player_data_json
+
+
 
     def get_player_stats(self):
         player_stats_request = requests.get(f'https://open.faceit.com/data/v4/players/{self.get_player_id()}/stats/{self.game_id}',headers=self.api_header)
         player_stats_json = player_stats_request.json()
-        print(player_stats_json)
+        return player_stats_json
 
 
 
 
-p = Faceit('Ultrafy')
 
 
-
-print(p.get_player_stats())
